77302d0f6fe230a78645561826a14709a4f5ed33
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2022-03-24 21:43:44 +0200

    Initialized react router

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import LoginPage from './pages/login-registerpage/login-registerpage.component';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <p>Hello world!</p>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Navigate to='/login' replace />} />
+          <Route exact path='/login' element={<LoginPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ view/src/pages/login-registerpage/login-registerpage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import LoginRegister from '../../components/login-register/login-register.component';
+
+import './login-registerpage.styles.css';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <LoginRegister />
+        </div>
+    )
+}
+
+export default LoginPage;
