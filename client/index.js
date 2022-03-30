b5e515e7f910c4e204dd6bccc9a54dedcb1f124b
Arsene Vlad <Arsuh@users.noreply.github.com>
2022-03-30 19:21:46 +0300

    reinitilaized client

--- client/index.js
+++
@@ -1,6 +0,0 @@
-const { User } = require('./Entities/user.js');
-const { db } = require('./firebaseConfig.js');
-
-
-let u = new User("XXXPaulXXX", "1234", "Paul", "a@a.a", "0123456789", "Address 7 Nr. 1", "50");
-console.log(u.getUsername(), u.getPassword());
