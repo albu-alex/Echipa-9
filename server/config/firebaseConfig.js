1ce54c205a27226222f42db49cbb59b78e34ac0a
Arsene Vlad <Arsuh@users.noreply.github.com>
2022-03-30 19:16:54 +0300

    updated firebaseConfig

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json') // ! Change this when merging
+const serviceAccount = require('./serviceAccount.json') // ! File ignored by git
 
 const { getFirestore } = require('firebase-admin/firestore');
 
@@ -7,4 +7,6 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://echipa-9-default-rtdb.europe-west1.firebasedatabase.app"
 });
+
+exports.admin = admin;
 exports.db = getFirestore();
