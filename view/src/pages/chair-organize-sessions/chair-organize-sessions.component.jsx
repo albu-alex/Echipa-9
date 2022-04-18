bc05c96a18a0c20864cfb0f67348a0e9d3fffb78
alexmhh <alexmhh1234@gmail.com>
2022-04-18 11:11:25 +0300

    Add PresentationCard component + page styling

--- view/src/pages/chair-organize-sessions/chair-organize-sessions.component.jsx
+++ view/src/pages/chair-organize-sessions/chair-organize-sessions.component.jsx
@@ -4,6 +4,7 @@ import { VariableSizeList as List } from 'react-window';
 
 import Header from '../../components/header/header.component';
 import Sidebar from '../../components/sidebar/sidebar.component';
+import PresentationCard from "../../components/presentation-card/presentation-card.component";
 
 import './chair-organize-sessions.styles.css';
 
@@ -13,8 +14,8 @@ const rowHeights = new Array(1000)
 
 const getItemSize = index => rowHeights[index];
 
-const Row = ({ index, style }) => (
-    <div style={style}>Row {index}</div>
+const Row = ({ style }) => (
+    <div style={style}><PresentationCard /></div>
 );
 
 const ChairOrganizeSessions = () => {
@@ -23,9 +24,25 @@ const ChairOrganizeSessions = () => {
             <Header />
             <Sidebar />
             <div className='chair-homepage'>
-                <List height={150} itemCount={1000} itemSize={getItemSize} width={300}>
-                    {Row}
-                </List>
+                <div className='session-list'>
+                    <a>
+                        Session 1
+                    </a>
+                    <a>
+                        Session 2
+                    </a>
+                    <a>
+                        Session 3
+                    </a>
+                    <a>
+                        Session 4
+                    </a>
+                </div>
+                <div className='list-container'>
+                    <List width={'100%'} height={250} className='list' itemCount={1000} itemSize={getItemSize}>
+                        {Row}
+                    </List>
+                </div>
                 <div className='buttons-container'>
                     <Link to='/chair-assign-paper'>
                         <button style={{width: '20vw'}} className='bigger'>See papers</button>
