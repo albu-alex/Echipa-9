4d35e16614b011395050efbbba46884b736c6d0c
Barabas Elina <elinabarabas2@gmail.com>
2022-05-06 20:44:45 +0300

    Review Paper for Chair ✔️

--- view/src/pages/chair-reviewpaper/chair-reviewpaper.component.jsx
+++ view/src/pages/chair-reviewpaper/chair-reviewpaper.component.jsx
@@ -7,6 +7,7 @@ import Scroll from "../../components/scroll/scroll.component";
 import CardList from "../../components/cardlist/cardlist.component";
 import {Link} from "react-router-dom";
 
+import './chair-reviewpaper.styles.css';
 
 const ChairReviewPaper = () => {
     return (
@@ -14,10 +15,50 @@ const ChairReviewPaper = () => {
             <Header/>
             <Sidebar/>
 
-            <div className='chair-homepage'>
+            <div className='details'>
+                <div class='form-style-5'>
+                    <form>
+                        <legend>Details</legend>
+                        <label>Name</label>
+                        <input type="text" name="name-field" placeholder="Text input..." />
+                        <label>Date</label>
+                        <input type="text" name="date-field" placeholder="Text input..." />
+                        <label>URL</label>
+                        <input type="text" name="url-field" placeholder="Text input..." />
+                        <label>Topics</label>
+                        <select id="topic" name="field4">
+                            <option value="health">Health</option>
+                            <option value="sports">Sports</option>
+                            <option value="history">History</option>
+                        </select>
+                    </form>
+                </div>
 
+                <div className='deadlines'>
+                    <div className='form-style-5'>
+                        <form>
+                            <legend>Deadlines</legend>
+                            <label>Paper submission</label>
+                            <input type="text" name="paper submission" placeholder="Date input..."/>
+                            <label>Paper review</label>
+                            <input type="text" name="paper review" placeholder="Date input..."/>
+                            <label>Accept papers</label>
+                            <input type="text" name="accept paper" placeholder="Date input..."/>
+                            <label>Upload camera-ready copies</label>
+                            <input type="text" name="camera-ready copies" placeholder="Date input..."/>
+                        </form>
+                    </div>
+                </div>
             </div>
+
+
+            <div className='form-style-5'>
+                <input type="submit" value="Save" required />
+            </div>
+
         </>
+
+
     )
 }
 
