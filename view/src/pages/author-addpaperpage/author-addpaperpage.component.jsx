8005a83c26a8d984fad49d9cc05db63b00896728
alexmhh <alexmhh1234@gmail.com>
2022-05-09 00:46:13 +0300

    Add request to submitPaper in author add paper page

--- view/src/pages/author-addpaperpage/author-addpaperpage.component.jsx
+++ view/src/pages/author-addpaperpage/author-addpaperpage.component.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/header/header.component';
 import Sidebar from '../../components/sidebar/sidebar.component';
+import { submitPaper } from "../../methods/submitPaper";
 
 import './author-addpaperpage.styles.css';
 
 const AuthorAddPaperPage = () => {
+    const [topic, setTopic] = useState("")
+    const [title, setTitle] = useState("")
+    const [coAuthor, setCoAuthor] = useState("")
+    const [keywords, setKeywords] = useState("")
+    const [resume, setResume] = useState("")
+    const changeTopic = (e) => {
+        setTopic(e.target.value);
+    }
+    const changeCoAuthor = (e) => {
+        setCoAuthor(e.target.value)
+    }
     return (
         <>
             <Header />
@@ -15,26 +27,28 @@ const AuthorAddPaperPage = () => {
                     <form>
                         <legend>Add your paper</legend>
                         <label>Paper Title *</label>
-                        <input type="text" name="field1" placeholder="required" required />
+                        <input onChange={text => setTitle(text)} type="text" name="field1" placeholder="required" required />
                         <label for="topic">Topic *</label>
-                        <select id="topic" name="field4">
+                        <select value={topic} onChange={changeTopic} id="topic" name="field4">
                             <option value="sports">Sports</option>
                             <option value="history">History</option>
                             <option value="health">Health</option>
                             <option value="more">More to come...</option>
                         </select>
                         <label for="co-authors">Co-authors</label>
-                        <select id="co-authors" name="field4">
+                        <select value={coAuthor} onChange={changeCoAuthor} id="co-authors" name="field4">
                             <option value="name1">Adam Paul</option>
                             <option value="name2">Albu Alex</option>
                             <option value="name3">Andrioaie Daria</option>
                             <option value="more">More to come...</option>
                         </select>
                         <label>Keywords *</label>
-                        <input type="text" name="field2" placeholder="required" required />
+                        <input onChange={text => setKeywords(text)} type="text" name="field2" placeholder="required" required />
                         <label>Abstract</label>
-                        <textarea name="field3" placeholder="Resume your paper"></textarea>
-                        <input type="submit" value="Submit Paper" required />
+                        <textarea onChange={text => setResume(text)} name="field3" placeholder="Resume your paper"></textarea>
+                        <button onClick={() => submitPaper(topic, title, coAuthor, keywords, resume)}>
+                            <p>Submit paper</p>
+                        </button>
                     </form>
                 </div>
                 <div className='upload-paper'>
