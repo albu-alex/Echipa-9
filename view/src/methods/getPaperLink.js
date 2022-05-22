7d41a2d66c242d168013aa237e1b6ca1973d2ff9
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2022-05-22 22:27:21 +0300

    Implement show paper in pdf format for reviewer

---
+++ view/src/methods/getPaperLink.js
@@ -0,0 +1,19 @@
+export async function getPaperLink() {
+    const authToken = localStorage.getItem('idToken');
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+
+    let link = "";
+    await fetch('/get-paper-link', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + authToken,
+            'paperId': id
+        }
+    }).then(response => { link = response.text() });
+
+    console.log(link);
+    return link;
+}
